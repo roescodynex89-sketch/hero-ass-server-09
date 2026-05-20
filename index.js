6d92695cd918f9a7b259e0bdc0590a4e94b2692d
roescodynex89-sketch <roescody.ne89x@gmail.com>
2026-05-20 23:36:25 +0600

    e error tumi thik hou

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const clientUrl=process.env.CLIENT_URL
 const uri = process.env.MONGO_DB_URI;
 const jwtSecret = process.env.JWT_SECRET;
-const clientUrl=process.env.CLIENT_URL
+
 
 // app.use(cors());
 app.use(cookieParser());
