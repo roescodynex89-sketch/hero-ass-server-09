2ef94685103cf75ce8fa6a477ad69bcd5b430b46
roescodynex89-sketch <roescody.ne89x@gmail.com>
2026-05-21 23:23:35 +0600

    add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL,"https://localhost:3000"],
     credentials: true,
   }),
 );
